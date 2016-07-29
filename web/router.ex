3c1d8a9b78cd0d53e2198f9d1b0c07f1634686f7
jasmo2 <jasmo2.com@gmail.com>
2016-07-29 18:15:11 -0500

    add api route

--- web/router.ex
+++ web/router.ex
@@ -13,14 +13,22 @@ defmodule Mula.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", Mula do
+    pipe_through :api
+
+    scope "/v1" do
+      post "/registrations/companies", RegistrationController, :create
+      post "/registrations/staff", RegistrationController, :create
+    end
+  end
+
   scope "/", Mula do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "*path", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Mula do
-  #   pipe_through :api
-  # end
 end
