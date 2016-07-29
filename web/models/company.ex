5d3fb19442bab2d83f2e5588526d36bc93d68525
jasmo2 <jasmo2.com@gmail.com>
2016-07-29 18:16:38 -0500

    refactor Company to Ecto 2.x

--- web/models/company.ex
+++ web/models/company.ex
@@ -9,15 +9,34 @@ defmodule Mula.Company do
     field :description, :string
     field :enabled, :boolean, default: true
 
+    field :password, :string, virtual: true
+
     timestamps()
   end
 
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
+  # @required_fields ~w(name enabled email password)
+  # @optional_fields ~w(slug description encrypted_password)
+
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:name, :url, :email, :description, :enabled])
-    |> validate_required([:name, :url, :email, :description, :enabled])
+    |> validate_required([:name, :email, :enabled, :password])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 5)
+    |> validate_confirmation(:password, message: "Las contraseñas no coinciden")
+    |> unique_constraint(:email, message: "El correo electronico ya está en uso")
+    |> generate_encrypted_password
+  end
+
+  defp generate_encrypted_password(current_changeset) do
+    case current_changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(current_changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+        current_changeset
+    end
   end
 end
